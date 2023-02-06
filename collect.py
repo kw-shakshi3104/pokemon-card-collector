bd1b1b5dcd5e8d00f91e5e03c0e1f9b1eca6d8db
kw-shakshi3104 <103113049+kw-shakshi3104@users.noreply.github.com>
2023-02-06 13:51:56 +0900

    画像を保存するコードを追加

--- collect.py
+++ collect.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession, AsyncHTMLSession
@@ -6,12 +7,14 @@ from requests_html import HTMLSession, AsyncHTMLSession
 import time
 from tqdm import tqdm
 
+from pathlib import Path
+
 
 # https://computer.masas-record-storage-container.com/2021/03/01/requestshtml/
 # https://it-syoya-engineer.com/python-requests-html/
 # https://qiita.com/uitspitss/items/f131ea79dffd58bc01ae
 
-def collect_cards(url: str) -> list:
+def collect_cards_async(url: str) -> list:
     # res = requests.get(url)
     # html = res.text
 
@@ -35,13 +38,38 @@ def collect_cards(url: str) -> list:
     return cards
 
 
+def download_image(url: str, output_filepath: Path):
+    res = requests.get(url)
+    img = res.content
+
+    with output_filepath.open("wb") as f:
+        f.write(img)
+
+
 if __name__ == "__main__":
     url = "https://www.pokemon-card.com/card-search/index.php?keyword=&se_ta=&regulation_sidebar_form=XY&pg=&illust=&sm_and_keyword=true"
-    # res = requests.get(url)
-    # html = res.text
 
+    # カード画像のURLのリストを取得
     cards = []
     last_page_idx = 120
     for i in tqdm(range(1, last_page_idx + 1)):
-        cards += collect_cards(url + f"&page={i}")
-        time.sleep(10)
+        cards = cards + collect_cards_async(url + f"&page={i}")
+        time.sleep(20)
+
+    # DataFrameに整形する
+    base_image_url = "https://www.pokemon-card.com"
+    cards = pd.DataFrame([{
+        "pokemon-name": card.attrs["alt"],
+        "image_url": base_image_url + card.attrs["src"],
+    } for card in cards])
+
+    # 画像名とカテゴリ(?)のURLから取得
+    path_urls = list(map(lambda x: Path(x), cards["image_url"]))
+    cards["image_name"] = list(map(lambda x: x.name, path_urls))
+    cards["expansion"] = list(map(lambda x: x.parts[-2], path_urls))
+
+    # 画像を取得
+    for img_url, img_name in tqdm(zip(cards["image_url"], cards["image_name"])):
+        output_filepath = Path(f"./images/{img_name}")
+        download_image(img_url, output_filepath)
+        time.sleep(1)
